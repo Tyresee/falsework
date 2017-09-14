29df2d2d477fb69e9073aed170989638d56af3fb
RainJoy <1490079545@qq.com>
2017-09-14 18:35:44 +0800

    react-demo

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.scss';
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="hello">
+          <span className="hello-world">hello</span>
+        </div>
       </div>
     );
   }
